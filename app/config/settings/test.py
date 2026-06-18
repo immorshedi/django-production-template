56db3720fc4c16cb8f03261f0e33aede44f984e9
mohammad morshedi <50455488+immorshedi@users.noreply.github.com>
2026-06-18 22:24:44 +0000

    refactor(settings): split Django settings by environment

--- app/config/settings/test.py
+++ app/config/settings/test.py
@@ -1,1 +1,9 @@
 from .base import *
+
+DEBUG = False
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+]
+
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
